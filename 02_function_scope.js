cf32f8b19a61f1e70ed160711814b3f40df242ff
Krushna <118190451+KrushnaMP03@users.noreply.github.com>
2023-12-24 04:12:31 +0000

    function_js

---
+++ 02_function_scope.js
@@ -0,0 +1,26 @@
+function addTwoValue(num1 , num2){
+    return num1+num2
+}
+console.log(addTwoValue(5,5));
+
+
+const number1 = (num1 , num2) => {
+    return num1+num2
+}
+console.log(number1(4,6));
+
+
+const number2 = () => console.log("hello");
+
+console.log(number2());
+
+
+((num1,num2)=>(console.log(num1+num2)))(6,4);
+
+const num =[1 , 2 , 3 , 4 , 5 , 8]
+// const val = num.filter((items) => items>2)
+
+//const val = num.map((items) => items + 10)
+
+const val = num.filter((items)=> items % 2 === 0).map((items) => items = items * 10)
+console.log(val);
